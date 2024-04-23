b26e9ee6f38dcc4abfb2cb4e62e4cd1ce4e9caff
Enrique García <enrique.garcia@redpontis.com>
2024-04-23 12:03:59 -0600

    refactor: 💡 aprendiendo async y await en DART
    \

---
+++ EJERCICIOS/04_tipos_nocomunes/05_async_await.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main(List<String> args) async {
+  String path =  Directory.current.path + '\\EJERCICIOS\\04_tipos_nocomunes\\assets\\personas.txt';
+
+  String texto =  await leerArchivo(path);
+  print(texto);
+
+  print('Fin del main');
+}
+
+
+Future leerArchivo(String path) async {
+
+  File file = new File( path );
+  return file.readAsString();
+  
+}
