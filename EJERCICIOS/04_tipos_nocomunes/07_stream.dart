571bd6847f9eb7f4b68461bc0feb6f87e7c557b8
Enrique García <enrique.garcia@redpontis.com>
2024-04-24 10:42:50 -0600

    refactor: 💡 streams, onError, OnDone, y cancelOnError

--- EJERCICIOS/04_tipos_nocomunes/07_stream.dart
+++ EJERCICIOS/04_tipos_nocomunes/07_stream.dart
@@ -10,11 +10,22 @@ void main(List<String> args) {
   ///en el cual el programa va a ejecutar esto 
   ///a destiempo cuando se resiva la informacion.
   // cuando resiva la informacion de este stream. son asincronos los stream
-  streamController.stream.listen((data) {
-    print('Despegando! $data');
-  });
+  streamController.stream.listen(
+    (data) => print('Despegando! $data'),
+    onError: (err) => print('Error! $err'),
+    onDone: () => print('Mision completa'), // se dispara cuando se ejecute el sink.close
+    cancelOnError: false // para cancelar el stream en caso salga un error
+  );
 
   streamController.sink.add('Apollo 11');
+  streamController.sink.add('Apollo 12');
+  streamController.sink.add('Apollo 13');
+  streamController.sink.addError('Houston, Tenemos un problema');
+  streamController.sink.add('Apollo 14');
+  streamController.sink.add('Apollo 15');
 
+  streamController.sink.close();
+
+  // streamController.sink.add('Apollo 16');
   print('Fin del main');
 }
