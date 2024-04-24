1cefee8c2c3a6da6a807e3e6094b6ffbf343bf92
Enrique García <enrique.garcia@redpontis.com>
2024-04-24 11:25:46 -0600

    refactor: 💡 optimizando la clase main

--- EJERCICIOS/05_clases/01_clase.dart
+++ EJERCICIOS/05_clases/01_clase.dart
@@ -4,10 +4,9 @@ void main(List<String> args) {
 
   final persona = new Persona();
 
-  persona.nombre = 'Enrique';
-  persona.edad = 28;
-  persona.bio = 'nacio de honduras';
+  persona..nombre = 'Enrique'
+         ..edad = 28
+         ..bio = 'nacio de honduras';
 
-  print(persona.toString());
+  print(persona);
 }
-
