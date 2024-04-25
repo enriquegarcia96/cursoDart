e87c52f23ec2e67b3faaf110e502d1365676f70e
Enrique García <enrique.garcia@redpontis.com>
2024-04-25 09:32:01 -0600

    refactor: 💡 constructores con nombre

--- EJERCICIOS/05_clases/01_clase.dart
+++ EJERCICIOS/05_clases/01_clase.dart
@@ -3,12 +3,14 @@ import 'clases/persona.dart';
 void main(List<String> args) {
 
   final persona = new Persona(edad: 28, nombre: 'Enrique');
-
+  final persona2 = new Persona.persona30('Andrea');
+  final persona40 = new Persona.persona40('Caceres');
+  final personaPersonalizado = new Persona.personalizado(edad: 80);
   // persona..nombre = 'Enrique'
   //        ..edad = 28;
         //  ..bio = 'nacio de honduras';
 
   // persona.bio = 'Cambio de valor';
 
-  print(persona);
+  print(personaPersonalizado);
 }
--- EJERCICIOS/05_clases/clases/persona.dart
+++ EJERCICIOS/05_clases/clases/persona.dart
@@ -25,6 +25,17 @@ class Persona {
   // tiene argumentos opcionales por nombres y valores por defecto
   Persona({ this.edad = 24, this.nombre = 'Andrea'});
 
+  // constructores con nombre
+  Persona.persona30( this.nombre ){
+    this.edad = 30;
+  }
+
+  Persona.persona40(String nombre){
+    this.edad = 40;
+    this.nombre = nombre;
+  }
+
+  Persona.personalizado({ this.edad = 18, this.nombre = 'Te falto el nombre' });
 
   
   // metodos
