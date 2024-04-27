d565edf1392dc7c25da30d80c2c120c6e4fb8d0c
Enrique García <enrique.garcia@redpontis.com>
2024-04-27 16:40:28 -0600

    refactor: 💡 mapear respuesta a una instancia de clase

--- paquetes/bin/paquetes.dart
+++ paquetes/bin/paquetes.dart
@@ -1,5 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+
 import 'package:paquetes/paquetes.dart' as paquetes;
 
-void main(List<String> arguments) {
-  print('Hello world: ${paquetes.calculate()}!');
+void main(List<String> arguments) async {
+
+  //https://reqres.in/api/users?page=2
+  var url  = Uri.https('reqres.in','/api/users',{'page': '2'} );
+  var response =  await  http.get(url).then((http.Response response) {
+    
+    final body = jsonDecode(response.body);
+    Iterable<dynamic> data = body['data'];
+    // print(body);
+    print('page: ${body['page']}');
+    print('per_page: ${body['per_page']}');
+    // print('id del tercer elemento: ${body['data'][2]['id']} ');
+    print('id del tercer elemento: ${data.elementAt(2)['id']}');
+  });
+
 }
