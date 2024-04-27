85db6514620234246a72e0d81826df8c3973967d
Enrique García <enrique.garcia@redpontis.com>
2024-04-27 13:49:49 -0600

    refactor: 💡 propiedades y metodos del double

--- EJERCICIOS/07_docs/01_double.dart
+++ EJERCICIOS/07_docs/01_double.dart
@@ -1,8 +1,23 @@
 void main(List<String> args) {
-  
   double numero = 3.1416;
+  double infinito = double.infinity;
 
   print('Firma: ${numero.sign} :: $numero');
+  print('isFinite: ${numero.isFinite} :: $numero');
+  print('isFinite: ${infinito.isFinite} :: $numero');
+
+  print('Firma: ${numero.abs()} :: $numero');
+  print('Firma: ${numero.ceil()} :: $numero');
+
+  // print('Firma: ${infinito.ceil()} :: $numero');
+
+  print('CeilToDouble: ${numero.ceilToDouble()} :: $numero');
+
+
+  print('roude: ${numero.round()} :: $numero');
+
+  print('clamp: ${numero.clamp(1, 3)} :: $numero');
+
 
 
 }
